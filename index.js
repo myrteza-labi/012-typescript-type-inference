c0c9a156d778a949fa114d2d2597231b7992ec01
myrteza <myrteza@hotmail.fr>
2023-10-07 15:24:40 +0200

    comiling in ts

--- index.js
+++ index.js
@@ -17,3 +17,19 @@ var items2 = [1, 2, null, 'Jhon'];
 document.addEventListener('click', function (e) {
     console.log(e.button);
 });
+/*
+En résumé, vous devriez généralement utiliser
+l'inférence de types autant que possible pour
+tirer parti des avantages de TypeScript en
+matière de flexibilité et de réduction de
+la duplication de code. Cependant, il peut
+être nécessaire d'utiliser des annotations
+de types dans des situations où TypeScript
+ne peut pas déduire correctement le type
+ou lorsque vous souhaitez rendre votre
+code plus explicite pour les autres
+développeurs ou pour des raisons de
+sécurité. L'utilisation appropriée
+des deux approches dépendra
+de la situation spécifique.
+*/ 
